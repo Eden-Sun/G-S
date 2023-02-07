8376415140b1714add900df5a60b46876434c914
eden sun <huntack478@gmail.com>
2023-02-08 02:09:12 +0800

    done till mda

---
+++ cert_jvqc.php
@@ -0,0 +1,23 @@
+﻿
+		<div style="background-image:url(./certs/JVQC_ONLINECOPY.jpg);no-repeat;position: relative;width:1285px;height:990px; solid black;float: left;" rel="imgtip[0]">
+		
+			<p style=" padding:370px 0px 0px 0px;"class="mid">
+				<b class="pvqcname"><?echo $rows[0];?></b>
+				<b class="pvqcnamect"><?echo $rows[1];?></b>
+			</p>
+			
+			<p style=" padding:30px 0px 0px 0px;"class="pvqcdate">
+				<?echo $rows[13];?>
+			</p>
+			
+			<p style ="padding:80px 0px 0px 0px;" class="pvqccer">
+				Japanese Vocabulary Quotient</p>
+			<p style=" padding:0px 0px 0px 0px;"class="pvqcctir">
+				<?echo $rows[4]."-".$rows[5];?>
+<br>
+				<?echo $rows[3];?><br>
+ <?echo $rows[6].$rows[7];?>
+			</p>										
+			<p style="padding:150px 0px 0px 1050px;" class="pvqcnum">				<?echo $rows[8];?></p>
+
+		</div>
---
+++ cert_mda.php
@@ -0,0 +1,25 @@
+﻿
+		<div style="background-image:url(./certs/MDA_ONLINECOPY.jpg);no-repeat;position: relative;width:1285px;height:990px; solid black;float: left;" rel="imgtip[0]">
+			<p style=" padding:370px 0px 0px 0px;"class="mid">
+				<b class="pvqcname"><?echo $rows[0];?>
+</b><b class="pvqcnamect"><?echo $rows[1];?></b>
+			</p>
+			
+			<p style=" padding:50px 0px 0px 0px;"class="pvqcdate">
+				<?echo $rows[13];?>
+			</p>
+			
+			<p style=" padding:120px 0px 0px 0px;" class="pvqccer">
+				 <?echo $rows[3];?>
+			</p>
+			<p style=" padding:0px 0px 0px 0px;"class="pvqcctir">
+				<?echo $rows[4]."-".$rows[5];?>
+				<br>
+				<?echo $rows[6].$rows[7];?>
+			</p>
+			<h4 class="pvqcnum">
+			<p style=" padding:150px 0px 0px 1050px;">
+				<?echo $rows[8];?>
+			</p></h4>
+		</div>
+	
