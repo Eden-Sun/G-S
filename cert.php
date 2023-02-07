2036e6a8e2883ad6086e9313c930f4f2e5d6caab
eden sun <huntack478@gmail.com>
2023-02-08 01:47:08 +0800

    done pvqc,vqc

--- cert.php
+++ cert.php
@@ -23,32 +23,11 @@
 		<?
 			include("db.php");
 			$cerno=$_POST['password'];
-			$cert;
 			$data=mysql_query("select * from info where Certificate ='$cerno'") ;
 			$rows = mysql_fetch_row($data);
+			$cert_id = $rows[15];
+			include("cert_".$cert_id.".php");
 		?>
-		<div style="background-image:url(./certs/PVQC_ONLINECOPY.jpg);no-repeat;position: relative;width:1285px;height:990px; solid black;float: left;" rel="imgtip[0]">
-			<p style=" padding:410px 0px 0px 0px;"class="mid">
-				<b class="pvqcname">   <?echo $rows[0];?>  </b>
-				<b class="pvqcnamect"> <?echo $rows[1];?>  </b>
-			</p>
-				
-			<p style=" padding:30px 0px 0px 0px;"class="pvqcdate">
-				<?echo $rows[13];?>
-			</p>
-				
-			<p style=" padding:80px 0px 0px 0px;" class="pvqccer">
-				English Vocabulary Quotient in <?echo $rows[3];?>
-			</p>
-			<p style=" padding:0px 0px 0px 0px;"class="pvqcctir">
-				<?echo $rows[4]."-".$rows[5];?>
-				<br>
-				<?echo $rows[6].$rows[7];?>
-			</p>
-			<h4 class="pvqcnum">
-				<p style=" padding:150px 0px 0px 1050px;"> <?echo $rows[8];?> </p>
-			</h4>
-		</div>
 	</body>
 	<script>
 		document.onmousedown=click;
---
+++ cert_vqc.php
@@ -0,0 +1,27 @@
+<div id="render_me" style="background-image:url(./certs/VQC_ONLINECOPY.jpg);no-repeat;position: relative;width:1285px;height:990px; solid black;float: left;" rel="imgtip[0]">
+			
+	<p style=" padding:350px 0px 0px 0px;"class="mid">
+		<b class="pvqcname"><?echo $rows[0];?></b>
+		<b class="pvqcnamect"><?echo $rows[1];?></b>
+	</p>
+	
+	<p style=" padding:30px 0px 0px 0px;"class="pvqcdate">
+		<?echo $rows[13];?>
+	</p>
+	
+	<p style=" padding:100px 0px 0px 0px;"class="pvqccer">
+		English Vocabulary Quotient
+	</p>
+	
+	<p style=" padding:0px 0px 0px 0px;"class="pvqcctir">
+		<?echo $rows[4]."-".$rows[5];?>
+	</p>
+	<p style=" padding:0px 0px 0px 0px;"class="pvqcctir">
+		<?echo $rows[3];?>
+	</p>
+	
+	<p style=" padding:150px 0px 0px 1100px;"class="pvqcnum">
+		<?echo $rows[8];?>
+	</p>
+
+</div>
