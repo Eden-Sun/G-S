b0d13efee0edb462d78dd7ac7752f021e0a1be27
eden sun <huntack478@gmail.com>
2023-02-08 02:05:28 +0800

    done tc

---
+++ cert_tcct.php
@@ -0,0 +1,22 @@
+
+<div style="background-image:url(./certs/COPYCT_typing.jpg);no-repeat;position: relative;width:700px;height:948px; solid black;float: left;" rel="imgtip[0]">
+	
+	<p style=" padding:300px 0px 0px 0px;"class="pvqcname">
+		<?echo $rows[0];?>
+	</p>
+	<p style=" padding:0px 0px 0px 0px;"class="pvqcnamect">
+		<?echo $rows[1];?>
+	</p>
+	<p style=" padding:0px 0px 0px 0px;"class="tcdate">
+		<?echo $rows[13];?>
+	</p>
+	
+	<p style=" padding:200px 0px 0px 0px;"class="tccer">
+		<?echo $rows[5]." ".$rows[4]; ?>
+	</p>
+	
+	<p style=" padding:140px 0px 0px 520px;"class="pvqcnum">
+		<?echo $rows[8];?>
+	</p>
+
+</div>
