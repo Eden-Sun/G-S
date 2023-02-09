b7156fbf75b93bd63f7a2b04e5ea9606dd43b098
eden sun <huntack478@gmail.com>
2023-02-09 16:38:48 +0800

    new certs

---
+++ cert_ict.php
@@ -0,0 +1,32 @@
+
+		<div style="background-image:url(./assets/ICT_ONLINECOPY.jpg);no-repeat;position: relative;width:1285px;height:990px; solid black;float: left;" rel="imgtip[0]">
+		
+			<p style=" padding:380px 0px 0px 0px;"class="ictname">
+				<?echo $rows[1]."&nbsp".$rows[0];?>
+			</p>
+			
+			<p style=" padding:45px 0px 0px 0px;"class="ictdate">
+				<?echo $rows[13];?>
+
+			</p>
+		
+			<p style=" padding:120px 0px 0px 0px;"class="ictcer">
+				-<?echo $rows[4];?> level
+
+			</p>
+		
+		
+			<p style=" padding:190px 0px 0px 1050px;"class="pvqcnum">
+				<?echo $rows[8];?>
+			</p>
+			<p style=" padding:0px 0px 0px 1050px;"class="pvqcnum">
+					<?
+					if   ($rows[14] != null)
+					echo "Validate Code:".$rows[14];
+					?>
+
+
+			</p>
+
+		</div>	
+	
