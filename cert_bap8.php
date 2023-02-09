20aa9a1a31f303dbcad7eb1250613aedeba8102e
eden sun <huntack478@gmail.com>
2023-02-09 16:25:53 +0800

    add bap

---
+++ cert_bap8.php
@@ -0,0 +1,31 @@
+
+		<div style="background-image:url(./assets/P-ALLMASTER.jpg);no-repeat;position: relative;width:1404px;height:993px; solid black;float: left;" rel="imgtip[0]">
+		
+			<p style="padding:410px 0px 0px 0px;"class="ictname">
+				<?echo $rows[0];?>
+			</p>
+
+
+			</p>
+		<p style=" padding:220px 0px 0px 0px;"class="bapdate">
+				Award on
+			</p>
+				<p style=" padding:0px 0px 0px 0px;"class="bapdate">
+				<?echo $rows[13];?>
+
+			</p>
+			
+			<p style=" padding:80px 0px 0px 1100px;"class="pvqcnum">
+				<?echo $rows[8];?>
+
+			</p>
+		<p style=" padding:0px 0px 0px 1100px;"class="pvqcnum">
+					Validate Code:<?echo $rows[14];?>
+
+
+
+		</div>
+
+
+
+		
